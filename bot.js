273ca197e88edce999acd269943dc2175b379bb6
Radu Vasilescu <vasilescur@gmail.com>
2020-02-17 16:41:42 -0500

    Improved functionality

--- bot.js
+++ bot.js
@@ -1,23 +1,33 @@
-var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
+var HTTPS = require("https");
+var cool = require("cool-ascii-faces");
 
 var botID = process.env.BOT_ID;
 
-const PLEDGES_TEXT = "pledges: @Andy Demma @baljeet charvu @Ben Fiszel @Brandon Weiss @David Ramirez @Feddy @Jason Huang @Mike Montelli @Mitchell Negus @Noah Citron @Raymond Lin @RJ Shah @Thuan Tran @Tyler Feldman";
-
+const PLEDGES_TEXT =
+  "pledges: @Andy Demma @baljeet charvu @Ben Fiszel @Brandon Weiss @David Ramirez @Feddy @Jason Huang @Mike Montelli @Mitchell Negus @Noah Citron @Raymond Lin @RJ Shah @Thuan Tran @Tyler Feldman";
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+  var request = JSON.parse(this.req.chunks[0]);
 
-  if(request.text && botRegex.test(request.text)) {
-    this.res.writeHead(200);
-    respondTo(request.text);
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
+  var at_pledges_regex = /@pledges/;
+  var bot_test_regex = /bot_test/;
+
+  botRegex = /^\/cool guy$/;
+
+  if (request.text) {
+    if (at_pledes_regex.test(request.text)) {
+      this.res.writeHead(200);
+      respondTo("@pledges");
+      this.res.end();
+    } else if (bot_test_regex.test(request.text)) {
+      this.res.writeHead(200);
+      respondTo("test");
+      this.res.end();
+    } else {
+      console.log("don't care");
+      this.res.writeHead(200);
+      this.res.end();
+    }
   }
 }
 
@@ -29,41 +39,39 @@ function respondTo(trigger) {
   }
 }
 
-
 function postMessage(msg) {
   var botResponse, options, body, botReq;
 
-  botResponse = msg;    // cool();
+  botResponse = msg; // cool();
 
   options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
+    hostname: "api.groupme.com",
+    path: "/v3/bots/post",
+    method: "POST"
   };
 
   body = {
-    "bot_id" : botID,
-    "text" : botResponse
+    bot_id: botID,
+    text: botResponse
   };
 
-  console.log('sending ' + botResponse + ' to ' + botID);
+  console.log("sending " + botResponse + " to " + botID);
 
   botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
+    if (res.statusCode == 202) {
+      //neat
+    } else {
+      console.log("rejecting bad status code " + res.statusCode);
+    }
   });
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
+  botReq.on("error", function(err) {
+    console.log("error posting message " + JSON.stringify(err));
   });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
+  botReq.on("timeout", function(err) {
+    console.log("timeout posting message " + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
 }
-
 
 exports.respond = respond;
